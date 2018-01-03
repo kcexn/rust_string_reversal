5540d3d54d024ec59ed418a1bf62b12cb8741c70
Kevin <068ant@gmail.com>
2018-01-03 22:43:07 +1100

    string reversal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut arguments = env::args();
+    arguments.next(); 
+  
+    let mut reversed_string = String::from("");
+
+    for mut argument in arguments {
+        while let Some(c) = argument.pop(){
+            reversed_string.push(c); 
+        }
+    }
+
+    println!("{}", reversed_string);
 }
